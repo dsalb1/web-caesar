aed47193d839810539d6e3af53e4af50daa24343
Dan Salberg <daniel.j.salberg@gmail.com>
2017-03-24 14:05:34 -0500

    All required functionalty complete. No CSS

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import webapp2
+import cgi
 
 add_header = """
 <!DOCTYPE HTML>
 <head>
-    <title>Web Caesar</title>
+	<title>Web Caesar</title>
 </head>
 <html>
 <body>
-    <h1>Web Caesar</h1>
-    <h3>Enter some text to encrypt</h3>
+	<h1>Web Caesar</h1>
+	<h3>Enter some text to encrypt</h3>
 """
 
 add_footer = """
@@ -19,13 +20,13 @@ new_message = ""
 
 message_form1 = """
 <form action="/encrypt" method="post" id="encrypt_form">
-    <textarea name="message" rows="5" cols="40" form="encrypt_form">"""
+	<textarea name="message" rows="5" cols="40" form="encrypt_form">"""
 
 message_form2="""
 </textarea>
-    <p><label>Enter how much you would like to rotate the letters</label></p>
-    <p><input type="text" name="rot"/></p>
-    <p><input type="submit" value="Encrypt"/></p>
+	<p><label>Enter how much you would like to rotate the letters</label></p>
+	<p><input type="text" name="rot"/></p>
+	<p><input type="submit" value="Encrypt"/></p>
 </form>
 """
 
@@ -34,13 +35,13 @@ def alphabet_position(letter):
 	return int(result)
 
 def rotate_character(char, rot):
-    rot = int(rot)
-    new_position = (alphabet_position(char) + rot) % 26
-    if char.isupper():
-        new_char = chr(new_position + 65)
-    else:
-        new_char = chr(new_position + 97)
-    return new_char
+	rot = int(rot)
+	new_position = (alphabet_position(char) + rot) % 26
+	if char.isupper():
+		new_char = chr(new_position + 65)
+	else:
+		new_char = chr(new_position + 97)
+	return new_char
 
 def encrypt(text, rot):
 	encrypted_text = ""
@@ -49,23 +50,24 @@ def encrypt(text, rot):
 			new_character = rotate_character(letter, rot)
 			encrypted_text += new_character
 		else:
+			letter = cgi.escape(letter)
 			encrypted_text += letter
 	return encrypted_text
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        content = add_header + message_form1 + new_message + message_form2 + add_footer
-        self.response.write(content)
+	def get(self):
+		content = add_header + message_form1 + new_message + message_form2 + add_footer
+		self.response.write(content)
 
 class AddMessage(webapp2.RequestHandler):
-    def post(self):
-        new_message = self.request.get("message")
-        rot = self.request.get("rot")
-        encrypted_message = encrypt(new_message, rot)
-        content = add_header + message_form1 + encrypted_message + message_form2 + add_footer
-        self.response.write(content)
+	def post(self):
+		new_message = self.request.get("message")
+		rot = self.request.get("rot")
+		encrypted_message = encrypt(new_message, rot)
+		content = add_header + message_form1 + encrypted_message + message_form2 + add_footer
+		self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ("/encrypt", AddMessage)
+	('/', MainHandler),
+	("/encrypt", AddMessage)
 ], debug=True)
